2eba924ebf28ea0abf4d6a557df2c72e11464334
kmbase <kmbase@hotmail.com>
2025-01-12 23:02:05 +0800

    fix script

--- android_nas_noroot@kmbase.sh
+++ android_nas_noroot@kmbase.sh
@@ -8,7 +8,8 @@ pkg install cloudflared -y
 pkg install alist -y
 pkg install aria2 -y
 
-# 创建配置文件目录
+# 创建配置文件目录，修正PREFIX变量赋值方式
+PREFIX="/data/data/com.termux/files/usr"
 CONFIG_DIR="$PREFIX/etc/"
 if [! -d "$CONFIG_DIR" ]; then
     mkdir -p "$CONFIG_DIR"
@@ -31,27 +32,16 @@ cloudflared_main="nohup cloudflared tunnel --no-autoupdate run $tunnel_token &"
 alist_main="nohup alist admin set $alist_password; nohup alist server --port 5244 &"
 aria2_main="nohup aria2c --enable-rpc --rpc-allow-origin-all &"
 
-cloudflared_cmd="
-if! pgrep -x "cloudflared" > /dev/null; then
-    $cloudflared_main
-fi
-"
-alist_cmd="
-if! pgrep -x "alist" > /dev/null; then
-    $alist_main"
-fi
-"
-aria2_cmd="
-if! pgrep -x "aria2" > /dev/null; then
-    $aria2_main"
-fi
-"
+cloudflared_cmd="if! pgrep -x 'cloudflared' > /dev/null; then $cloudflared_main; fi"
+alist_cmd="if! pgrep -x 'alist' > /dev/null; then $alist_main; fi"
+aria2_cmd="if! pgrep -x 'aria2' > /dev/null; then $aria2_main; fi"
+
 # 追加到 termux-login.sh
 echo "$cloudflared_cmd" >> $PREFIX/etc/termux-login.sh
 echo "$alist_cmd" >> $PREFIX/etc/termux-login.sh
 echo "$aria2_cmd" >> $PREFIX/etc/termux-login.sh
 
 # 启动服务
-eval $cloudflared_main
-eval $alist_cmd
-eval $aria2_cmd
+eval "$cloudflared_main"
+eval "$alist_main"
+eval "$aria2_main"
