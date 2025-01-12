5d6c949f8538eac55d11f20870f869235d015e7d
kmbase <kmbase@hotmail.com>
2025-01-13 00:47:09 +0800

    update

--- android_nas_noroot@kmbase.sh
+++ android_nas_noroot@kmbase.sh
@@ -9,23 +9,15 @@ pkg install alist -y
 pkg install aria2 -y
 
 # 定义变量
-PREFIX="~/usr"
-LOGFILE="~/etc/nohup.out"
-
-# 创建配置文件目录，修正PREFIX变量赋值方式
-CONFIG_DIR="$PREFIX/etc/"
-if [ ! -d "$CONFIG_DIR" ]; then
-    mkdir -p "$CONFIG_DIR"
-fi
-CONFIG_FILE="$CONFIG_DIR/config.txt"
+PREFIX="~/usr/etc"
 
 # 获取用户输入的关键信息
 read -p "set tunnel token of cloudflare: " tunnel_token
 read -p "set admin password of alist: " alist_password
 
-cloudflared_main="nohup cloudflared tunnel --no-autoupdate run --token $tunnel_token > $LOGFILE 2>&1 &"
-alist_main="nohup alist admin set $alist_password; nohup alist server > $LOGFILE 2>&1 &"
-aria2_main="nohup aria2c --enable-rpc --rpc-allow-origin-all > $LOGFILE 2>&1 &"
+cloudflared_main="nohup cloudflared tunnel --no-autoupdate run --token $tunnel_token > "$PREFIX/nohup.out" 2>&1 &"
+alist_main="nohup alist admin set $alist_password; nohup alist server > "$PREFIX/nohup.out" 2>&1 &"
+aria2_main="nohup aria2c --enable-rpc --rpc-allow-origin-all > "$PREFIX/nohup.out" 2>&1 &"
 
 # 构建启动命令字符串，先判断服务是否运行
 cloudflared_cmd="if pgrep -x 'cloudflared' >/dev/null
@@ -51,9 +43,9 @@ aria2_cmd="if pgrep -x 'aria2' >/dev/null
 fi"
 
 # 追加到 termux-login.sh
-echo "$cloudflared_cmd" >> $PREFIX/etc/termux-login.sh
-echo "$alist_cmd" >> $PREFIX/etc/termux-login.sh
-echo "$aria2_cmd" >> $PREFIX/etc/termux-login.sh
+echo "$cloudflared_cmd" >> $PREFIX/termux-login.sh
+echo "$alist_cmd" >> $PREFIX/termux-login.sh
+echo "$aria2_cmd" >> $PREFIX/termux-login.sh
 
 # 启动服务
 eval "$cloudflared_main"
