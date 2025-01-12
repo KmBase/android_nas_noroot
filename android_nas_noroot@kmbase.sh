c89c8689094528a53b9d839ebe8ce2a81ab88d15
kmbase <kmbase@hotmail.com>
2025-01-13 00:21:56 +0800

    update

--- android_nas_noroot@kmbase.sh
+++ android_nas_noroot@kmbase.sh
@@ -20,14 +20,7 @@ CONFIG_FILE="$CONFIG_DIR/config.txt"
 read -p "set tunnel token of cloudflare: " tunnel_token
 read -p "set admin password of alist: " alist_password
 
-# 写入配置文件
-echo "tunnel_token=$tunnel_token" > "$CONFIG_FILE"
-echo "alist_password=$alist_password" >> "$CONFIG_FILE"
-
-# 读取配置信息
-source "$CONFIG_FILE"
-
-cloudflared_main="nohup cloudflared tunnel --no-autoupdate run $tunnel_token &"
+cloudflared_main="nohup cloudflared tunnel --no-autoupdate run -token $tunnel_token &"
 alist_main="nohup alist admin set $alist_password; nohup alist server &"
 aria2_main="nohup aria2c --enable-rpc --rpc-allow-origin-all &"
 
@@ -63,3 +56,5 @@ echo "$aria2_cmd" >> $PREFIX/etc/termux-login.sh
 eval "$cloudflared_main"
 eval "$alist_main"
 eval "$aria2_main"
+
+
